fb7cfba959228b93ec313100ff292a566ac469e8
Christian Loos <cloos@netcologne.de>
2011-05-20 22:51:21 +0200

    update copyright

--- lib/RT/Extension/HistoryFilter.pm
+++ lib/RT/Extension/HistoryFilter.pm
@@ -62,7 +62,7 @@ Christian Loos <cloos@netsandbox.de>
 
 =head1 COPYRIGHT AND LICENCE
  
-Copyright (C) 2010, Christian Loos.
+Copyright (C) 2010-2011, Christian Loos.
  
 This library is free software; you can redistribute it and/or modify
 it under the same terms as Perl itself.
